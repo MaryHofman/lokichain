6a0ae5e60c4743bb1b775bb31f667401cb489dc5
Mary22222222 <64154351+Mary22222222@users.noreply.github.com>
2024-11-02 15:51:24 +0300

    create a basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,6 @@
-trait  animal {
-    fn speak(&self);
-}
 
-struct dog{
-    name: String,
-}
 
-impl dog {
-    fn new(name: &str) -> dog {
-        dog {
-            name: name.to_string(),
-        }
-    }
-}
-
-impl animal for dog {
-
-    
-    fn speak(&self) {
-        print!("Wow")
-    }
-}
 fn main() {
-   let x= dog::new("bobby");
-    x.speak();
-  
+    print!("Hello")
 }
 
