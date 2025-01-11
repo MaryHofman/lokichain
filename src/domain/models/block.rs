d435611f136317e988cae25ba3570234ec0e9883
JKearnsl <boyarshinovn@gmail.com>
2025-01-11 17:13:15 +0300

    update: fields, added creation logic

--- src/domain/models/block.rs
+++ src/domain/models/block.rs
@@ -1,18 +1,63 @@
+use chrono::Utc;
+
 use super::transaction::Transaction;
+use crate::domain::models::address::Address;
+use crate::domain::models::hash::Hash;
+use crate::domain::models::signature::Signature;
+use crate::domain::models::token::Token;
+use serde::{Deserialize, Serialize};
 
-
+#[derive(Serialize, Deserialize, Clone)]
 pub struct BlockHeader {
-    version: u32,
-    height: u32,
-    timestamp: i64,
-    pre_hash: String,
-    merkle_root: String,
-    difficulty: u32,
-    nonce: u32,
+    /// block hash
+    hash: Hash,
+    /// Block number
+    height: u64,
+    /// Time of creation
+    timestamp: u64,
+    /// hash of the previous block
+    pre_hash: Hash,
+    /// merkle root transaction hash
+    merkle_root: Hash,
+    /// The address of the validator that issued this block
+    validator: Address,
+    /// the reward this validator received for issuing a block
+    reward: Token,
+    /// validator signature
+    signature: Signature
 }
 
+
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Block {
     header: BlockHeader,
-    count: u32,
     transactions: Vec<Transaction>
+}
+
+
+impl Block {
+    pub fn new(
+        hash: Hash,
+        height: u64,
+        pre_hash: Hash,
+        merkle_root: Hash,
+        validator: Address,
+        reward: Token,
+        sign: Signature,
+        transactions: Vec<Transaction>
+    ) -> Self {
+        Block {
+            header: BlockHeader {
+                hash,
+                height,
+                pre_hash,
+                merkle_root,
+                validator,
+                reward,
+                signature: sign,
+                timestamp: Utc::now().timestamp() as u64,
+            },
+            transactions
+        }
+    }
 }
--- src/domain/models/transaction.rs
+++ src/domain/models/transaction.rs
@@ -1,16 +1,58 @@
-pub enum TransactionStatus {
-    Pending,
-    Confirmed,
-    Failed,
+use chrono::Utc;
+use crate::domain::models::address::Address;
+use crate::domain::models::hash::Hash;
+use crate::domain::models::signature::Signature;
+use crate::domain::models::token::Token;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Transaction {
+    /// Transaction hash (not including signature)
+    pub hash: Hash,
+    /// Address of the sender
+    pub sender: Address,
+    /// Data field (for smart contract calls or arbitrary logic)
+    pub data: Vec<u8>,
+    /// Amount of tokens to transfer (if applicable)
+    pub amount: Token,
+    /// Timestamp of the transaction
+    pub timestamp: u64,
+    /// The commission (gas) value is calculated as *gas_price* * *gas_limit*
+    ///
+    /// * gas_price - the price of calculation, depends on the network load
+    /// * gas_limit - the maximum amount of "gas" that the user is willing
+    /// to spend to perform a transaction
+    ///
+    /// The more gas, the faster the transaction will be included in the block
+    pub gas: u64,
+    /// Sender transaction number
+    ///
+    /// This number is stored in the state. It is important to keep the transactions
+    /// in order and make them unique to protect against replay
+    pub nonce: u64,
+    /// Signature of the transaction
+    pub signature: Signature
 }
 
-pub struct Transaction {
-    pub sender: String,
-    pub receiver: String,
-    pub amount: f64,
-    pub timestamp: u64,
-    pub signature: Option<String>,
-    pub fee: f64,
-    pub nonce: u64,
-    pub status: TransactionStatus,
+impl Transaction {
+    pub fn new(
+        hash: Hash,
+        sender: Address,
+        data: Vec<u8>,
+        amount: Token,
+        gas: u64,
+        nonce: u64,
+        signature: Signature
+    ) -> Self {
+        Transaction {
+            hash,
+            sender,
+            data,
+            amount,
+            timestamp: Utc::now().timestamp() as u64,
+            gas,
+            nonce,
+            signature
+        }
+    }
 }
